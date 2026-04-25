fda73a13d89d36591ff3a27db99131862a7577f2
Wills007-deep <willsbusiness88@gmail.com>
2026-04-25 15:38:48 +0100

    Fix syntax error dashboard

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -321,8 +321,7 @@ export default function Dashboard() {
               </div>
            </div>
         </div>
-        {/* Dashboard Header */}>
-        </div>
+
         {/* Dashboard Header */}
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-8 gap-4">
           <div>
